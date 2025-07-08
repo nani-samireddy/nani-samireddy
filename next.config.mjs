eda24d13053bc34c0c586df91e346aba945211c8
nani-samireddy <nanisamireddy05@gmail.com>
2025-07-08 21:21:08 +0530

    chore update nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const isLocal = process.env.ENV === 'local';
 
 if (!isLocal) {
 	nextConfig.basePath = process.env.BASE_PATH || '/nani-samireddy';
+	nextConfig.assetPrefix = process.env.BASE_PATH || '/nani-samireddy';
+	nextConfig.publicRuntimeConfig = {
+		basePath: process.env.BASE_PATH || '/nani-samireddy',
+	};
 }
 
 export default nextConfig;
